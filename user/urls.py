42a9b5d6f8ba62252f109e7e3c15675d174b6c2b
Harrybo68 <harry.esslinger@gmail.com>
2023-12-19 10:07:52 +0100

    ptit fix

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(template_name="user/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_form/', views.change_password, name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import get_user_model, login, update_session_auth_hash
 from django.contrib import messages
 
 def register(request):
@@ -32,3 +33,21 @@ def register(request):
 @login_required
 def dashboard(request):
     return render(request, 'user/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'user/password_change_form.html', {
+        'form': form
+    })
