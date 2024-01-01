7578144a3d314756d6ac22d76d0738893e58e9f4
Harrybo68 <harry.esslinger@gmail.com>
2024-01-01 20:46:37 +0100

    j'ai fini toute la partie js mais le css est à revoir, je suis pas très doué xD

--- user/static/user/js/login-validation.js
+++ user/static/user/js/login-validation.js
@@ -1,29 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('password-change-form');
+    var form = document.getElementById('login-form');
     var button = document.getElementById('button');
     var validationMessage = document.getElementById('validation-message');
 
     function updateButtonState() {
-        var oldPassword = form.querySelector('#id_old_password').value;
-        var newPassword1 = form.querySelector('#id_new_password1').value;
-        var newPassword2 = form.querySelector('#id_new_password2').value;
+        var Username = form.querySelector('#id_username').value;
+        var Password = form.querySelector('#id_password').value;
         var allFilled = Array.from(form.querySelectorAll('.input-area')).every(function(input) {
             return input.value.trim() !== '';
         });
-        var isValid1 = newPassword1.length >= 8;
-        var isValid2 = newPassword1 === newPassword2;
-        var isValid3 = newPassword1 !== oldPassword;
+        var isValid = Password.length >= 8;
 
-        if (allFilled && isValid1 && isValid2 && isValid3) {
+        if (allFilled && isValid) {
             button.classList.remove('disabled-button');
             button.disabled = false;
-            validationMessage.textContent = 'Password is valid';
+            validationMessage.textContent = 'form seems correct';
             validationMessage.classList.remove('invalid');
             validationMessage.classList.add('valid');
         } else {
             button.classList.add('disabled-button');
             button.disabled = true;
-            validationMessage.textContent = 'Password isn\'t valid';
+            validationMessage.textContent = 'username and password are required';
             validationMessage.classList.remove('valid');
             validationMessage.classList.add('invalid');
         }
--- user/static/user/js/registration-validation.js
+++ user/static/user/js/registration-validation.js
@@ -1,29 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('password-change-form');
+    var form = document.getElementById('registration-form');
     var button = document.getElementById('button');
     var validationMessage = document.getElementById('validation-message');
 
     function updateButtonState() {
-        var oldPassword = form.querySelector('#id_old_password').value;
-        var newPassword1 = form.querySelector('#id_new_password1').value;
-        var newPassword2 = form.querySelector('#id_new_password2').value;
+        var Password1 = form.querySelector('#id_password1').value;
+        var Password2 = form.querySelector('#id_password2').value;
         var allFilled = Array.from(form.querySelectorAll('.input-area')).every(function(input) {
             return input.value.trim() !== '';
         });
-        var isValid1 = newPassword1.length >= 8;
-        var isValid2 = newPassword1 === newPassword2;
-        var isValid3 = newPassword1 !== oldPassword;
+        var isValid1 = Password1.length >= 8;
+        var isValid2 = Password1 === Password2;
 
-        if (allFilled && isValid1 && isValid2 && isValid3) {
+        if (allFilled && isValid1 && isValid2) {
             button.classList.remove('disabled-button');
             button.disabled = false;
-            validationMessage.textContent = 'Password is valid';
+            validationMessage.textContent = 'form seems correct';
             validationMessage.classList.remove('invalid');
             validationMessage.classList.add('valid');
         } else {
             button.classList.add('disabled-button');
             button.disabled = true;
-            validationMessage.textContent = 'Password isn\'t valid';
+            validationMessage.textContent = 'form is incomplete or invalid';
             validationMessage.classList.remove('valid');
             validationMessage.classList.add('invalid');
         }
